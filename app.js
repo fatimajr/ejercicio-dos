789f64374dbda5770a74cf294267d6994777fe5e
fatima <fatima.jacoboruiz@gmail.com>
2016-08-25 14:29:17 -0500

    ejercicio js dos

---
+++ app.js
@@ -0,0 +1,4 @@
+var num1 = parseInt(prompt("Teclea el primer número"));
+var num2 = parseInt(prompt("Teclea el segundo número"));
+var num3 = parseInt(prompt("Teclea el tercer número"));
+document.write("El promedio es: " + (num1 + num2 + num3)/3);
